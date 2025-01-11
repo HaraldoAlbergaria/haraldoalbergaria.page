9ef65944ce1e28ff7ce4c21b622f4e9dbbbcc911
HaraldoFilho <haraldo.filho@gmail.com>
2025-01-11 09:02:12 -0300

    [auto] Updated Flickr Photos Map

--- map/countries.py
+++ map/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
+  'PY': ['Paraguay', 1, 1],
+  'AR': ['Argentina', 93, 176],
+  'VA': ['Vatican City', 22, 31],
+  'BR': ['Brazil', 176, 394],
+  'CL': ['Chile', 78, 198],
+  'HR': ['Croatia', 184, 243],
   'US': ['United States', 50, 132],
   'UY': ['Uruguay', 36, 67],
-  'CL': ['Chile', 78, 198],
-  'BR': ['Brazil', 176, 394],
-  'PY': ['Paraguay', 1, 1],
-  'IT': ['Italy', 77, 126],
-  'VA': ['Vatican City', 22, 31],
-  'AR': ['Argentina', 93, 176],
-  'HR': ['Croatia', 184, 243]
+  'IT': ['Italy', 77, 126]
 }
--- map/user.py
+++ map/user.py
@@ -4,6 +4,8 @@ user_info = {
   'name': 'Haraldo Albergaria',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/30622510@N02_r.jpg',
   'url': 'https://www.flickr.com/photos/hpfilho/',
-  'markers': 527,
-  'photos': 955
+  'location': 'Campinas, Brazil',
+  'countries': 9,
+  'markers': 717,
+  'photos': 1368
 }
