8b9763558f14c4e2eda235fc85769ed5ce603bd7
Haraldo Filho <haraldo.filho@gmail.com>
2026-08-26 14:43:07 -0300

    Updated About Me page with new Camera and Lenses

--- data/gear.js
+++ data/gear.js
@@ -1,11 +1,15 @@
 var cam_body = [
-  'Canon EOS Rebel T5i'
+  "Canon EOS Rebel T5i",
+  "Canon EOS R10"
 ]
 
 var zoom_lens = [
   "Canon EF-S 10-18mm f/4.5-5.6 IS STM",
   "Canon EF-S 18-55mm f/3.5-5.6 IS STM",
-  "Canon EF-S 55-250mm f/4-5.6 IS STM"
+  "Canon EF-S 55-250mm f/4-5.6 IS STM",
+  "Canon RF-S 10-18mm f/4.5-6.3 IS STM",
+  "Canon RF-S 18-45mm f/4.5-6.3 IS STM"
+
 ]
 
 var prime_lens = [
