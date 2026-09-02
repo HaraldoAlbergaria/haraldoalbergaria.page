b53dbf45865f9b81d9abc38b855a4d823cc1ee89
Haraldo Filho <haraldo.filho@gmail.com>
2026-09-02 17:55:15 -0300

    Updated gear lists

--- data/gear.js
+++ data/gear.js
@@ -1,18 +1,19 @@
 var cam_body = [
-  "Canon EOS Rebel T5i",
-  "Canon EOS R10"
+  "Canon EOS R10",
+  "Canon EOS Rebel T5i"
 ]
 
 var zoom_lens = [
+  "Canon RF-S 10-18mm f/4.5-6.3 IS STM",
+  "Canon RF-S 18-45mm f/4.5-6.3 IS STM",
   "Canon EF-S 10-18mm f/4.5-5.6 IS STM",
   "Canon EF-S 18-55mm f/3.5-5.6 IS STM",
-  "Canon EF-S 55-250mm f/4-5.6 IS STM",
-  "Canon RF-S 10-18mm f/4.5-6.3 IS STM",
-  "Canon RF-S 18-45mm f/4.5-6.3 IS STM"
+  "Canon EF-S 55-250mm f/4-5.6 IS STM"
 
 ]
 
 var prime_lens = [
+  "Canon RF 16mm f/2.8 STM",
   "Canon EF-S 24mm f/2.8 STM",
   "Canon EF 50mm f/1.8 STM"
 ]
